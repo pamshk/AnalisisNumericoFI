cbb83944e4e25e6276387a244b81f987c875b26b
pamshk <71911127+pamshk@users.noreply.github.com>
2020-11-06 21:30:56 -0600

    Primera clase de análisis numérico
    Se vieron los errores absoluto, relativo y porcentual; así como la notación científica en python y operaciones matemáticas básicas.

---
+++ ErrorAbsoluto-Relativo-Porcentual.py
@@ -0,0 +1,20 @@
+#Notación cientifica en Python
+print(1e3) #1x10^3
+print(2.5e3) # ->2500
+
+#Declaramos variables
+Viga = 1000 #10 metros
+VigaAproximada = 1050 # medida aproximada
+
+#Se obtiene el error absoluto con la formula
+#Error Absoluto = |(Valor Real - Valor aproximado)|
+EA = abs(viga - vigaAproximada)
+print('El error absoluto es {EA}')
+
+#Obtenemos el error relativo
+ER = EA / abs(viga)
+print('El Error relativo es {ER}')
+
+#Obtenemos el error porcentual
+ERP = ER *100
+print('El error Relativo Porcentual es {ERP}')
